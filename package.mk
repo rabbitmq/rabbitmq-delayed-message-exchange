fbc764ab6bd782925c7f7e34e8784e9351fd4e2d
Alvaro Videla <videlalvaro@gmail.com>
2015-03-30 21:01:08 +0200

    adds first tests

--- package.mk
+++ package.mk
@@ -1,3 +1,4 @@
 DEPS:=rabbitmq-server rabbitmq-erlang-client
-
-
+RETAIN_ORIGINAL_VERSION:=true
+WITH_BROKER_TEST_COMMANDS:=rabbit_exchange_type_delayed_message_test:test()
+WITH_BROKER_TEST_CONFIG:=$(PACKAGE_DIR)/etc/rabbit-test
---
+++ test/src/rabbit_exchange_type_delayed_message_test.erl
@@ -0,0 +1,330 @@
+-module(rabbit_exchange_type_delayed_message_test).
+
+-export([test/0]).
+
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("amqp_client/include/amqp_client.hrl").
+
+-define(RABBIT, {"rabbit-test",  5672}).
+-define(HARE,   {"rabbit-hare", 5673}).
+
+-import(rabbit_exchange_type_delayed_message_test_util,
+        [start_other_node/1, cluster_other_node/2,
+         reset_other_node/1, stop_other_node/1]).
+
+test() ->
+    ok = eunit:test(tests(?MODULE, 60), [verbose]).
+
+delay_order_test() ->
+    {ok, Conn} = amqp_connection:start(#amqp_params_network{}),
+    {ok, Chan} = amqp_connection:open_channel(Conn),
+
+    Ex = <<"e1">>,
+    Q = <<"q">>,
+
+    setup_fabric(Chan, make_exchange(Ex, <<"direct">>), make_queue(Q)),
+
+    Msgs = [5000, 1000, 3000, 2000, 1000, 4000],
+
+    publish_messages(Chan, Ex, Msgs),
+
+    Result = consume(Chan, Q, Msgs),
+
+    Sorted = lists:sort(Msgs),
+    {ok, Sorted} = Result,
+
+    ok.
+
+make_queue(Q) ->
+    #'queue.declare' {
+       queue       = Q
+      }.
+
+make_durable_queue(Q) ->
+    #'queue.declare' {
+       queue       = Q,
+       durable     = true,
+       auto_delete = false
+      }.
+
+make_exchange(Ex, Type) ->
+    #'exchange.declare' {
+       exchange    = Ex,
+       type        = <<"x-delayed-message">>,
+       arguments   = [{<<"x-delayed-type">>,
+                       longstr, Type}]
+      }.
+
+make_durable_exchange(Ex, Type) ->
+    #'exchange.declare' {
+       exchange    = Ex,
+       type        = <<"x-delayed-message">>,
+       durable     = true,
+       auto_delete = false,
+       arguments   = [{<<"x-delayed-type">>,
+                       longstr, Type}]
+      }.
+
+make_msg(V) ->
+    #amqp_msg{props = #'P_basic'{
+                         headers = make_h(V)},
+              payload = term_to_binary(V)}.
+
+make_h(V) ->
+    [{<<"x-delay">>, signedint, V}].
+
+setup_fabric(Chan,
+             ExDeclare = #'exchange.declare'{exchange = Ex},
+             QueueDeclare) ->
+    #'exchange.declare_ok'{} =
+        amqp_channel:call(Chan, ExDeclare),
+
+    #'queue.declare_ok'{queue = Q} =
+        amqp_channel:call(Chan, QueueDeclare),
+
+    #'queue.bind_ok'{} =
+        amqp_channel:call(Chan, #'queue.bind' {
+                                   queue = Q,
+                                   exchange = Ex
+                                  }).
+
+publish_messages(Chan, Ex, Msgs) ->
+        [amqp_channel:call(Chan,
+                           #'basic.publish'{exchange = Ex},
+                           make_msg(V)) || V <- Msgs].
+
+consume(Chan, Q, Msgs) ->
+    #'basic.consume_ok'{} =
+        amqp_channel:subscribe(Chan, #'basic.consume'{queue  = Q,
+                                                      no_ack = true}, self()),
+    collect(length(Msgs), lists:max(Msgs) + 1000).
+
+
+collect(N, Timeout) ->
+    collect(0, N, Timeout, []).
+
+collect(N, N, _Timeout, Acc) ->
+    {ok, lists:reverse(Acc)};
+collect(Curr, N, Timeout, Acc) ->
+    receive {#'basic.deliver'{},
+             #amqp_msg{payload = Bin}} ->
+            collect(Curr+1, N, Timeout, [binary_to_term(Bin) | Acc])
+    after Timeout ->
+            {error, {timeout, Acc}}
+    end.
+
+%% default_length_test() ->
+%%     Qs = qs(),
+%%     test0(fun () ->
+%%                   #'basic.publish'{exchange = <<"e">>}
+%%           end,
+%%           fun() ->
+%%                   #amqp_msg{props = #'P_basic'{}, payload = <<>>}
+%%           end, [], Qs, 100, length(Qs) * ?KEEP_NB).
+
+%% length_argument_test() ->
+%%     Qs = qs(),
+%%     test0(fun () ->
+%%                   #'basic.publish'{exchange = <<"e">>}
+%%           end,
+%%           fun() ->
+%%                   #amqp_msg{props = #'P_basic'{}, payload = <<>>}
+%%           end, [{<<"x-recent-history-length">>, long, 30}], Qs, 100, length(Qs) * 30).
+
+%% wrong_argument_type_test() ->
+%%     wrong_argument_type_test0(-30),
+%%     wrong_argument_type_test0(0).
+
+
+%% no_store_test() ->
+%%     Qs = qs(),
+%%     test0(fun () ->
+%%                   #'basic.publish'{exchange = <<"e">>}
+%%           end,
+%%           fun() ->
+%%                   H = [{<<"x-recent-history-no-store">>, bool, true}],
+%%                   #amqp_msg{props = #'P_basic'{headers = H}, payload = <<>>}
+%%           end, [], Qs, 100, 0).
+
+%% e2e_test() ->
+%%     MsgCount = 10,
+%%     {ok, Conn} = amqp_connection:start(#amqp_params_network{}),
+%%     {ok, Chan} = amqp_connection:open_channel(Conn),
+
+%%     #'exchange.declare_ok'{} =
+%%         amqp_channel:call(Chan,
+%%                           #'exchange.declare' {
+%%                             exchange = <<"e1">>,
+%%                             type = <<"x-recent-history">>,
+%%                             auto_delete = true
+%%                            }),
+
+%%     #'exchange.declare_ok'{} =
+%%         amqp_channel:call(Chan,
+%%                           #'exchange.declare' {
+%%                             exchange = <<"e2">>,
+%%                             type = <<"direct">>,
+%%                             auto_delete = true
+%%                            }),
+
+%%     #'queue.declare_ok'{queue = Q} =
+%%         amqp_channel:call(Chan, #'queue.declare' {
+%%                                    queue     = <<"q">>
+%%                                   }),
+
+%%     #'queue.bind_ok'{} =
+%%         amqp_channel:call(Chan, #'queue.bind' {
+%%                                    queue = Q,
+%%                                    exchange = <<"e2">>,
+%%                                    routing_key = <<"">>
+%%                                   }),
+
+%%     #'tx.select_ok'{} = amqp_channel:call(Chan, #'tx.select'{}),
+%%     [amqp_channel:call(Chan,
+%%                        #'basic.publish'{exchange = <<"e1">>},
+%%                        #amqp_msg{props = #'P_basic'{}, payload = <<>>}) ||
+%%         _ <- lists:duplicate(MsgCount, const)],
+%%     amqp_channel:call(Chan, #'tx.commit'{}),
+
+%%     amqp_channel:call(Chan,
+%%                       #'exchange.bind' {
+%%                          source      = <<"e1">>,
+%%                          destination = <<"e2">>,
+%%                          routing_key = <<"">>
+%%                         }),
+
+%%     #'queue.declare_ok'{message_count = Count, queue = Q} =
+%%         amqp_channel:call(Chan, #'queue.declare' {
+%%                                    passive   = true,
+%%                                    queue     = Q
+%%                                   }),
+
+%%     ?assertEqual(MsgCount, Count),
+
+%%     amqp_channel:call(Chan, #'exchange.delete' { exchange = <<"e1">> }),
+%%     amqp_channel:call(Chan, #'exchange.delete' { exchange = <<"e2">> }),
+%%     amqp_channel:call(Chan, #'queue.delete' { queue = Q }),
+%%     amqp_channel:close(Chan),
+%%     amqp_connection:close(Conn),
+%%     ok.
+
+%% multinode_test() ->
+%%     start_other_node(?HARE),
+%%     cluster_other_node(?HARE, ?RABBIT),
+
+%%     {ok, Conn} = amqp_connection:start(#amqp_params_network{port=5673}),
+%%     {ok, Chan} = amqp_connection:open_channel(Conn),
+
+%%     #'exchange.declare_ok'{} =
+%%         amqp_channel:call(Chan,
+%%                           #'exchange.declare' {
+%%                             exchange = <<"e1">>,
+%%                             type = <<"x-recent-history">>,
+%%                             auto_delete = false
+%%                            }),
+
+%%     #'queue.declare_ok'{queue = Q} =
+%%         amqp_channel:call(Chan, #'queue.declare' {
+%%                                    queue     = <<"q">>
+%%                                   }),
+
+%%     #'queue.bind_ok'{} =
+%%         amqp_channel:call(Chan, #'queue.bind' {
+%%                                    queue = Q,
+%%                                    exchange = <<"e1">>,
+%%                                    routing_key = <<"">>
+%%                                   }),
+
+%%     amqp_channel:call(Chan, #'queue.delete' { queue = Q }),
+%%     amqp_channel:close(Chan),
+%%     amqp_connection:close(Conn),
+%%     stop_other_node(?HARE),
+
+%%     {ok, Conn2} = amqp_connection:start(#amqp_params_network{}),
+%%     {ok, Chan2} = amqp_connection:open_channel(Conn2),
+
+%%     #'queue.declare_ok'{queue = Q2} =
+%%         amqp_channel:call(Chan2, #'queue.declare' {
+%%                                    queue     = <<"q2">>
+%%                                   }),
+
+%%     #'queue.bind_ok'{} =
+%%         amqp_channel:call(Chan2, #'queue.bind' {
+%%                                    queue = Q2,
+%%                                    exchange = <<"e1">>,
+%%                                    routing_key = <<"">>
+%%                                   }),
+
+%%     amqp_channel:call(Chan2, #'exchange.delete' { exchange = <<"e2">> }),
+%%     amqp_channel:call(Chan2, #'queue.delete' { queue = Q2 }),
+%%     amqp_channel:close(Chan2),
+%%     amqp_connection:close(Conn2),
+%%     ok.
+
+%% test0(MakeMethod, MakeMsg, DeclareArgs, Queues, MsgCount, ExpectedCount) ->
+%%     {ok, Conn} = amqp_connection:start(#amqp_params_network{}),
+%%     {ok, Chan} = amqp_connection:open_channel(Conn),
+%%     #'exchange.declare_ok'{} =
+%%         amqp_channel:call(Chan,
+%%                           #'exchange.declare' {
+%%                             exchange = <<"e">>,
+%%                             type = <<"x-recent-history">>,
+%%                             auto_delete = true,
+%%                             arguments = DeclareArgs
+%%                            }),
+
+%%     #'tx.select_ok'{} = amqp_channel:call(Chan, #'tx.select'{}),
+%%     [amqp_channel:call(Chan,
+%%                        MakeMethod(),
+%%                        MakeMsg()) || _ <- lists:duplicate(MsgCount, const)],
+%%     amqp_channel:call(Chan, #'tx.commit'{}),
+
+%%     [#'queue.declare_ok'{} =
+%%          amqp_channel:call(Chan, #'queue.declare' {
+%%                              queue = Q, exclusive = true }) || Q <- Queues],
+
+%%     [#'queue.bind_ok'{} =
+%%          amqp_channel:call(Chan, #'queue.bind' { queue = Q,
+%%                                                  exchange = <<"e">>,
+%%                                                  routing_key = <<"">>})
+%%      || Q <- Queues],
+
+%%     Counts =
+%%         [begin
+%%             #'queue.declare_ok'{message_count = M} =
+%%                  amqp_channel:call(Chan, #'queue.declare' {queue     = Q,
+%%                                                            exclusive = true }),
+%%              M
+%%          end || Q <- Queues],
+
+
+%%     ?assertEqual(ExpectedCount, lists:sum(Counts)),
+
+%%     amqp_channel:call(Chan, #'exchange.delete' { exchange = <<"e">> }),
+%%     [amqp_channel:call(Chan, #'queue.delete' { queue = Q }) || Q <- Queues],
+%%     amqp_channel:close(Chan),
+%%     amqp_connection:close(Conn),
+%%     ok.
+
+%% wrong_argument_type_test0(Length) ->
+%%     {ok, Conn} = amqp_connection:start(#amqp_params_network{}),
+%%     {ok, Chan} = amqp_connection:open_channel(Conn),
+%%     DeclareArgs = [{<<"x-recent-history-length">>, long, Length}],
+%%     process_flag(trap_exit, true),
+%%     ?assertExit(_, amqp_channel:call(Chan,
+%%                           #'exchange.declare' {
+%%                             exchange = <<"e">>,
+%%                             type = <<"x-recent-history">>,
+%%                             auto_delete = true,
+%%                             arguments = DeclareArgs
+%%                             })),
+%%     ok.
+
+%% qs() ->
+%%     [<<"q0">>, <<"q1">>, <<"q2">>, <<"q3">>].
+
+tests(Module, Timeout) ->
+    {foreach, fun() -> ok end,
+     [{timeout, Timeout, fun () -> Module:F() end} ||
+         {F, _Arity} <- proplists:get_value(exports, Module:module_info()),
+         string:right(atom_to_list(F), 5) =:= "_test"]}.
