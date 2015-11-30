c3df52f2a07e74b87a1388c8de36d789250bd7a4
Michael Klishin <michael@clojurewerkz.org>
2015-11-30 19:16:27 +0300

    rabbit_misc:now_to_ms/1 is gone

--- src/rabbit_delayed_message.erl
+++ src/rabbit_delayed_message.erl
@@ -160,7 +160,7 @@ maybe_delay_first(CurrTimer) ->
         %% destructuring to prevent matching '$end_of_table'
         #delay_key{timestamp = FirstTS} = Key2 ->
             %% there are messages that expired and need to be delivered
-            Now = rabbit_misc:now_to_ms(os:timestamp()),
+            Now = time_compat:erlang_system_time(milli_seconds),
             start_timer(FirstTS - Now, Key2);
         _ ->
             %% nothing to do
@@ -176,7 +176,7 @@ route(#delay_key{exchange = Ex}, Deliveries) ->
               end, Deliveries).
 
 internal_delay_message(CurrTimer, Exchange, Delivery, Delay) ->
-    Now = rabbit_misc:now_to_ms(os:timestamp()),
+    Now = time_compat:erlang_system_time(milli_seconds),
     %% keys are timestamps in milliseconds,in the future
     DelayTS = Now + Delay,
     mnesia:dirty_write(?INDEX_TABLE_NAME,
