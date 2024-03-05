60bedacdba6cbc208034e01e7ba30f5cffe3d76f
Luke Bakken <luke@bakken.io>
2024-03-05 15:54:03 -0800

    Update for RabbitMQ 3.13
    Fixes #263
    
    Address @pjk25 review comment
    
    Remove OTP 25 since RMQ 3.13 does NOT support it.
    
    Apply fix proposed by @dumbbell
    
    update mk files
    
    Remove 3.11

--- src/rabbit_exchange_type_delayed_message.erl
+++ src/rabbit_exchange_type_delayed_message.erl
@@ -48,7 +48,7 @@ route(X = #exchange{name = Name},
             %% route the message using proxy module
             case ?EXCHANGE(X) of
                 rabbit_exchange_type_direct ->
-                    RKs = mc:get_annotation(routing_keys, Message),
+                    RKs = mc:routing_keys(Message),
                     %% Exchange type x-delayed-message routes via "direct exchange routing v1"
                     %% even when feature flag direct_exchange_routing_v2 is enabled because
                     %% table rabbit_index_route only stores bindings whose source exchange
